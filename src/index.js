f0698d71414e8e8aca1e85dd519e35d0524e1107
Raghav Kanwal <raghav.kanwal@gmail.com>
2022-05-21 13:06:00 +0530

    dynamic keys in react using nanoid

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: false },
+    { id: "todo-1", name: "Sleep", completed: true },
+    { id: "todo-2", name: "Repeat", completed: false }
+  ];
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
-root.render(<App subject="Clarice" />);
+root.render(<App tasks={DATA} />);
