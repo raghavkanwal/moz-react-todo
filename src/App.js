5fe77b914319ac6cc6cfaa271ea173b9c1c95d45
Raghav Kanwal <raghav.kanwal@gmail.com>
2022-05-22 12:27:19 +0530

    Filtering the list of tasks.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
+
+  const [filter, setFilter] = useState('All');
+  const [tasks, setTasks] = useState(props.tasks);
+
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name: name, completed: false};
     setTasks([...tasks, newTask]);
   }
   
-  const [tasks, setTasks] = useState(props.tasks);
   
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
@@ -37,15 +48,25 @@ function App(props) {
     setTasks(newTasks);
   }
 
-  const taskList = tasks.map(task => (
-    <Todo
-      key={task.id}
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      toggleTaskCompleted={toggleTaskCompleted}
-      deleteTask={deleteTask}
-      editTask={editTask}
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+      ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+      key={name} name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -57,9 +78,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
 
       <h2 id="list-heading">
